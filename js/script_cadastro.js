7ef5dc65e57dddff8fd9782a7e665593a8a8dd54
rffantin <rffantin@gmail.com>
2023-07-12 16:52:58 -0300

    fix: correcao de problemas na validacao do form

--- js/script_cadastro.js
+++ js/script_cadastro.js
@@ -1,43 +1,35 @@
-//Inputs e tratamentos necessários
-// nome -> ok, não precisa de verificação
-// email -> ok, verificação automática pelo type do input
-// senha -> verificar minimo de caracteres
-// confirmação de senha -> verificar se senha1 = senha2
-// RG -> verificação de RG
-// CEP -> completar automaticamente pelo API e trata erros 
-//        campos: estado, cidade, bairro, rua, num, complemento
-
 // ------------------ DECLARAÇÃO DE VARIAVEIS ----------------------------------------
 const senha = document.getElementById('inputSenha')
 const confirmaSenha = document.getElementById('inputConfirmaSenha')
 const formulario = document.getElementById('formulario')
-//const email = document.getElementById('inputEmail')
+const email = document.getElementById('inputEmail')
 const cep = document.getElementById('inputCEP')
 const estado = document.getElementById('inputEstado')
 const cidade = document.getElementById('inputCidade')
 const bairro = document.getElementById('inputBairro')
 const rua = document.getElementById('inputRua')
-//const numero = document.getElementById('inputNum')
-//const complemento = document.getElementById('inputComplemento')
+const numero = document.getElementById('inputNum')
+const complemento = document.getElementById('inputComplemento')
 //const formInputs = document.querySelectorAll('.form-control')
 const valorBotao = document.getElementById('envioFormulario')
-const valorSenha1 = 0
-const valorSenha2 = 0
+const nome = document.getElementById('inputNome')
+let valorSenha1 = 0
+let valorSenha2 = 0
 
 // ----------------- FUNÇÕES / OBJETOS / CLASSES -----------------------------------
 
 const trataCEP = {
-    limpaFormulario: function(){
+    limpaFormulario: function () {
         formulario.reset()
     },
-    msgErro: function(){
+    msgErro: function () {
         alert("CEP inválido, tente novamente!")
     }
 
 }
 
 const trataSenha = {
-    estiloMinCaracteres: function (senhaDigitada, senha){
+    estiloMinCaracteres: function (senhaDigitada, senha) {
         for (let i = 1; i < senhaDigitada.length; i++) {
             senha.style.backgroundColor = "red"
             if (senhaDigitada.length === 8) {
@@ -48,23 +40,23 @@ const trataSenha = {
         }
     },
 
-    estiloComparaSenhas: function(senha2Digitada, confirmaSenha, senha){
-        if(senha2Digitada != senha.value){
+    estiloComparaSenhas: function (senha2Digitada, confirmaSenha, senha) {
+        if (senha2Digitada != senha.value) {
             confirmaSenha.style.backgroundColor = 'red'
         }
-        else{
+        else {
             confirmaSenha.style.backgroundColor = 'white'
         }
     },
-    validaEnvio: function(senha, confirmaSenha){
-        if(senha.value === confirmaSenha.value){
+    validaEnvio: function (senha, confirmaSenha) {
+        if (senha.value === confirmaSenha.value) {
             return alert('cadastro realizado')
         }
-        else{
+        else {
             return alert('tente novamente')
         }
     }
-    
+
 
 }
 
@@ -73,19 +65,47 @@ const trataSenha = {
 senha.addEventListener("keyup", function (event) {
     const senhaDigitada = event.target.value //indica o elemento que disparou o evento
     trataSenha.estiloMinCaracteres(senhaDigitada, senha)
-} 
+}
 )
 
- 
-confirmaSenha.addEventListener("keyup", function(event){
+
+confirmaSenha.addEventListener("keyup", function (event) {
     const senha2Digitada = event.target.value
     trataSenha.estiloComparaSenhas(senha2Digitada, confirmaSenha, senha)
 })
 
-valorBotao.addEventListener("submit", function (evento) {
-    evento.preventDefault() //retira o envio padrão do formulario
-    //------------ falta tratar o caso de senhas serem diferentes ou com menos de 8 caracteres
+valorBotao.addEventListener("click", function (evento) {
+    evento.preventDefault()
+    const campos = [senha.value, cep.value, confirmaSenha, estado.value, cidade.value, bairro.value, rua.value, email.value, numero.value, complemento.value, nome.value]
+    console.log(campos)
+    const itensFiltrados = campos.filter((campo) => campo.length == 0)
+
+    console.log(itensFiltrados)
+    if (senha.length < 8) {
+        alert("A senha precisa ter pelo menos 8 caracteres.")
+    } else if (senha.value != confirmaSenha.value) {
+        alert("As senhas digitadas são diferentes, tente novamente.")
+    }else if(itensFiltrados.length > 0){
+    alert("Por favor, preencha todos os campos.")
+    }else {
+        senha.value = ''
+        confirmaSenha.value = ''
+        cep.value = ''
+        estado.value = ''
+        cidade.value = ''
+        bairro.value = ''
+        rua.value = ''
+        email.value = ''
+        numero.value = ''
+        complemento.value = ''
+        nome.value = ''
+        alert("Cadastro realizado com sucesso!")
+
+    }
+
 })
+
+
 
 
 // -------------------- REQUISIÇÃO API: Endereço ---------------------------------------------------------------------
@@ -101,22 +121,27 @@ cep.addEventListener("keyup", function (event) {
 
 })
 
-const buscaEndereco = async (cep) => {
-    
-    const urlAPI = `https://viacep.com.br/ws/${cep}/json` //template string uma vez que a API buscará o valor do cep armazenado em uma variável
+const buscaEndereco = async (cepValor) => {
+
+    const urlAPI = `https://viacep.com.br/ws/${cepValor}/json` //template string uma vez que a API buscará o valor do cep armazenado em uma variável
     const resposta = await fetch(urlAPI) // o fetch envia uma requisição GET para a URL especificada. O await foi utilizado uma vez que trata-se de uma função assincrona, ele aguarda a conclusão de uma promesa antes de prosseguir a execução do código. 
     const dados = await resposta.json() //recebe os dados do endereço
-    //console.log(dados)
+    console.log(dados)
 
     //---------- falta tratar o erro para caso cep não exista
-
-
-
-    // Autocompleta os inputs 
-    estado.value = dados.uf
-    cidade.value = dados.localidade
-    bairro.value = dados.bairro
-    rua.value = dados.logradouro
+    if (dados && dados.erro) {
+        alert("Cep inválido, digite novamente.")
+        cep.value = ''
+    } else {
+        estado.value = dados.uf
+        cidade.value = dados.localidade
+        bairro.value = dados.bairro
+        rua.value = dados.logradouro
+    }
 }
+
+
+
+
 
 
